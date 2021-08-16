9768c0f47eeaf8f49d603d6ccd15dd2fa26fe295
Everton Faria de Oliveira <everton.faria@outlook.com>
2021-08-16 10:53:33 -0300

    Ajustes nas bibliotecas importadas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,8 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Cadastros.Models;
 using System;
 using Microsoft.AspNetCore.Authorization;
-using System.Linq;
 using Cadastros.Services;
 using Cadastros.Repositories;
 
